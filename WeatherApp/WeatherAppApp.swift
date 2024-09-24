60732e4d6977fb54b6a118f84e0da638768c0411
Gerardo Gallegos <36527267+gallegosg@users.noreply.github.com>
2024-09-23 18:27:27 -0700

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Gerardo Gallegos on 9/23/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
